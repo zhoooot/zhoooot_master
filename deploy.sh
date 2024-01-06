2272bd5fad8d846e9c9b99015fa31d128e5bbbb5
ntl22 <nxtung21@outlook.com>
2024-01-06 23:20:59 +0700

    3 services

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
 docker-compose -f server-auth/docker-compose.yaml up -d
+docker-compose -f server-creator/docker-compose.yaml up -d
--- shutdown.sh
+++ shutdown.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
 docker-compose -f server-auth/docker-compose.yaml down
+docker-compose -f server-creator/docker-compose.yaml down
